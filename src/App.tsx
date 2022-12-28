0145390916e6758ce849408cb60a5135dbec3d55
Craig Fisk <fisk.craig@gmail.com>
2022-12-28 23:54:05 +0000

    Added user list route and configured react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { LoginPage } from "./pages/login";
+import { UserListPage } from "./pages/userList";
 
 const theme = createTheme();
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/users",
+    element: <UserListPage />,
+  },
+]);
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <LoginPage />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
---
+++ src/pages/userList/index.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from "react";
+import Container from "@mui/material/Container";
+import { Button, Card, CardContent, Grid, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import { Info, Delete } from "@mui/icons-material";
+import { UserList } from "./types";
+
+async function fetchData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return data as UserList;
+}
+
+export function UserListPage() {
+  const [userList, setUserList] = useState<UserList>([]);
+
+  useEffect(() => {
+    fetchData().then((data) => {
+      setUserList(data);
+    });
+  }, []);
+
+  return (
+    <Container component="main">
+      {userList &&
+        userList.map((user) => {
+          return (
+            <Card sx={{ marginBottom: "24px" }}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {user.name}
+                </Typography>
+                <Grid container spacing={3}>
+                  <Grid item xs="auto">
+                    <Typography variant="subtitle2" component="p">
+                      Username:
+                    </Typography>
+                    <Typography>{user.username}</Typography>
+                  </Grid>
+                  <Grid item xs="auto">
+                    <Typography variant="subtitle2" component="p">
+                      Fullname:
+                    </Typography>
+                    <Typography>{user.name}</Typography>
+                  </Grid>
+                  <Grid item xs="auto">
+                    <Typography variant="subtitle2" component="p">
+                      Email Address:
+                    </Typography>
+                    <Typography>{user.email}</Typography>
+                  </Grid>
+                  <Grid item xs="auto">
+                    <Typography variant="subtitle2" component="p">
+                      Actions:
+                    </Typography>
+                    <Stack direction="row" spacing={2}>
+                      <Button
+                        variant="contained"
+                        startIcon={<Info />}
+                        size="small"
+                      >
+                        Details
+                      </Button>
+                      <Button
+                        variant="outlined"
+                        startIcon={<Delete />}
+                        size="small"
+                      >
+                        Delete
+                      </Button>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          );
+        })}
+    </Container>
+  );
+}
